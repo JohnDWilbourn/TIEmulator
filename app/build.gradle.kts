9bf9b76e29c1b3d16dc226f95d1b08f653686655
John David Wilbourn <john.david.wilbourn@gmail.com>
2025-07-22 17:55:37 -0600

    Initial working version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.maynard.scientificcalculator"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.maynard.scientificcalculator"
